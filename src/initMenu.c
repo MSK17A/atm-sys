a09a3d7ef5812351cb4fabc86f38d52bea2cef2a
Mohammed AlSammak <msk170a@gmail.com>
2023-08-18 17:10:22 +0300

    .

--- src/initMenu.c
+++ src/initMenu.c
@@ -1,21 +1,18 @@
 #include "../headers/addUserMenu.h"
 #include "../headers/loginMenu.h"
+#include "../headers/mainMenu.h"
 #include "../headers/sql_operations.h"
 #include "../headers/user_struct.h"
-#include "../headers/mainMenu.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void initMenu_Instructions_Print();
+
 void initMenu(sqlite3 *db, User *user) {
   int r = 0;
   int option;
-  system("clear");
-  printf("\n\n\t\t======= ATM =======\n");
-  printf("\n\t\t-->> Feel free to login / register :\n");
-  printf("\n\t\t[1]- login\n");
-  printf("\n\t\t[2]- register\n");
-  printf("\n\t\t[3]- exit\n");
+  initMenu_Instructions_Print();
   while (!r) {
     scanf("%d", &option);
     switch (option) {
@@ -23,6 +20,7 @@ void initMenu(sqlite3 *db, User *user) {
       /* If user logged in successfully let him enters mainMenu */
       if (loginMenu(db, user) == 1) {
         mainMenu(db, user);
+        return;
       } else {
         exit(1);
       }
@@ -41,3 +39,12 @@ void initMenu(sqlite3 *db, User *user) {
     }
   }
 };
+
+void initMenu_Instructions_Print() {
+  system("clear");
+  printf("\n\n\t\t======= ATM =======\n");
+  printf("\n\t\t-->> Feel free to login / register :\n");
+  printf("\n\t\t[1]- login\n");
+  printf("\n\t\t[2]- register\n");
+  printf("\n\t\t[3]- exit\n");
+}
